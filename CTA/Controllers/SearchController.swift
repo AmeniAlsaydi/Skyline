d857ce185a6f32f6fe71022f48c28479d2f3f555
Ameni Alsaydi <amenialsaydi@pursuit.org>
2020-03-16 15:58:45 -0400

    created eventCell

--- CTA/Controllers/SearchController.swift
+++ CTA/Controllers/SearchController.swift
@@ -10,9 +10,41 @@ import UIKit
 
 class SearchController: UIViewController {
 
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureCollectionView()
 
     }
+    
+    private func configureCollectionView() {
+        collectionView.register(EventCell.self, forCellWithReuseIdentifier: "eventCell")
+        collectionView.dataSource = self
+        collectionView.delegate = self
+    }
 
+}
+
+extension SearchController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "eventCell", for: indexPath) as? EventCell else {
+            fatalError("could not down cast to event cell")
+        }
+        return cell
+    }
+}
+
+extension SearchController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        let maxSize = UIScreen.main.bounds
+        let height = maxSize.height * 0.15
+        let width = maxSize.width
+        return CGSize(width: width, height: height)
+    }
 }
