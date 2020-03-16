ce925b2445bf4f6f2799a7be7fd5fb569d7d3287
Ameni Alsaydi <amenialsaydi@pursuit.org>
2020-03-16 18:36:35 -0400

    can retrieve user experience and change collection cell accordingly

--- CTA/Controllers/SearchController.swift
+++ CTA/Controllers/SearchController.swift
@@ -12,32 +12,46 @@ class SearchController: UIViewController {
 
     @IBOutlet weak var collectionView: UICollectionView!
     
-    private var appState = AppState.events {
+    private var appState: AppState = .art {
         didSet {
+            configureCollectionView()
             // reload collection view
-            // call configureCollectionView()
+            DispatchQueue.main.async {
+                self.collectionView.reloadData()
+            }
+            
+             
         }
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        configureCollectionView()
-        test()
+        getUserExperience()
+        //configureCollectionView()
 
     }
     
     private func configureCollectionView() {
-        collectionView.register(EventCell.self, forCellWithReuseIdentifier: "eventCell")
+        
+        if appState == .art {
+            collectionView.register(ArtCell.self, forCellWithReuseIdentifier: "artCell")
+        } else if appState == .events {
+            collectionView.register(EventCell.self, forCellWithReuseIdentifier: "eventCell")
+        }
+
         collectionView.dataSource = self
         collectionView.delegate = self
     }
     
     
-    private func test() {
+    private func getUserExperience() {
         DatabaseService.shared.getUserExperience { [weak self] (result) in
             switch result {
             case .failure(let error):
-                //completion(.failure(error))
                 print("ERROR HERE: \(error.localizedDescription)")
             case .success(let experience):
                 print("user experience: \(experience)")
@@ -57,10 +71,21 @@ extension SearchController: UICollectionViewDataSource {
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "eventCell", for: indexPath) as? EventCell else {
-            fatalError("could not down cast to event cell")
+        
+        
+        if appState == .events {
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "eventCell", for: indexPath) as? EventCell else {
+                fatalError("could not down cast to event cell")
+            }
+            return cell
+        } else if appState == .art {
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "artCell", for: indexPath) as? ArtCell else {
+                fatalError("could not down cast to art cell")
+            }
+            return cell
         }
-        return cell
+   
+        return UICollectionViewCell()
     }
 }
 
@@ -68,8 +93,20 @@ extension SearchController: UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         
         let maxSize = UIScreen.main.bounds
-        let height = maxSize.height * 0.15
-        let width = maxSize.width
+        
+        var height: CGFloat!
+        var width: CGFloat!
+        
+        if appState == .art {
+            height = maxSize.height * 0.25
+            width = maxSize.width * 0.45
+        } else if appState == .events {
+            height = maxSize.height * 0.13
+            width = maxSize.width
+        }
+        
         return CGSize(width: width, height: height)
     }
+    
+    
 }
